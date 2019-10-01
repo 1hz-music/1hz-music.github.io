6ab1388afc19d7e6897c2f3e01e57b306e3abc98
Jimmy Ben Klieve <j.b.klieve@live.cn>
2019-10-01 15:58:31 +0000

    update website

---
+++ precache-manifest.e6581de2720230681bbfc0a1d3e412a6.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5e28766090faa23b6ba20834f851b81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36eb277cfba18e049495",
+    "url": "/static/css/2.ac1ce0ba.chunk.css"
+  },
+  {
+    "revision": "9949846693a821aaaa08",
+    "url": "/static/css/main.5f4cff10.chunk.css"
+  },
+  {
+    "revision": "36eb277cfba18e049495",
+    "url": "/static/js/2.b4e63f1e.chunk.js"
+  },
+  {
+    "revision": "9949846693a821aaaa08",
+    "url": "/static/js/main.ea91e3e1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0e442b7e934b5070ae0e525d1f382029",
+    "url": "/static/media/1hz.0e442b7e.svg"
+  },
+  {
+    "revision": "46d60e77ece2fca494388cbdc7c8bf08",
+    "url": "/static/media/1hz_white.46d60e77.svg"
+  },
+  {
+    "revision": "0883815f4d2e84a7c534b3333f1e8a57",
+    "url": "/static/media/cover.0883815f.jpg"
+  },
+  {
+    "revision": "4b3d7dfc702b2422a48f08f9de9c101c",
+    "url": "/static/media/cover.4b3d7dfc.jpg"
+  },
+  {
+    "revision": "5d5c744e4c15402e4758a78e2e8395ea",
+    "url": "/static/media/cover.5d5c744e.jpg"
+  },
+  {
+    "revision": "9b89eb98fbbde21a05f8c4c7ae4deeb2",
+    "url": "/static/media/cover.9b89eb98.jpg"
+  },
+  {
+    "revision": "9e65fbcf5bff7f8dd33d167539446368",
+    "url": "/static/media/cover.9e65fbcf.jpg"
+  },
+  {
+    "revision": "e24cc2776c7ffc606505b8a011dc007d",
+    "url": "/static/media/gugugu.e24cc277.jpg"
+  },
+  {
+    "revision": "980dd9ab8e8721953089ea0c9833a2a6",
+    "url": "/static/media/jumbotron.980dd9ab.jpg"
+  },
+  {
+    "revision": "4d87c31bb86642c95831cbbeba2d63ec",
+    "url": "/static/media/munro-webfont.4d87c31b.ttf"
+  },
+  {
+    "revision": "60f3f86bf836dccb76cd61ffec1de913",
+    "url": "/static/media/munro-webfont.60f3f86b.woff2"
+  },
+  {
+    "revision": "ab66f86ae5c48c61d968e2dbec2af385",
+    "url": "/static/media/munro-webfont.ab66f86a.svg"
+  },
+  {
+    "revision": "aff91c64083bc7d5bd5561235e84fe06",
+    "url": "/static/media/munro-webfont.aff91c64.eot"
+  },
+  {
+    "revision": "e79fe0f1f9ee23f16b4d3fdb44741a2c",
+    "url": "/static/media/munro-webfont.e79fe0f1.woff"
+  },
+  {
+    "revision": "1d7f26ae89f2621386a4e847b403ce8b",
+    "url": "/static/media/tokyo_trc_10_27.1d7f26ae.jpg"
+  }
+]);
